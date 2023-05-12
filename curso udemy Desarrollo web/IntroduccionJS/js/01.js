61c97836ab89d60b87061ab0cf1261779f9316e9
William Emmanuel Mazariego Orellana <83888568+Will-Trucker@users.noreply.github.com>
2023-05-11 23:52:04 -0600

    udemy js

---
+++ curso udemy Desarrollo web/IntroduccionJS/js/01.js
@@ -0,0 +1,24 @@
+// Variables
+
+var producto = 'Audifonos Gamer'; //Iniciar la Variable y asignarle valor
+
+var disponible; //Iniciar variable pero sin ningun valor
+
+//producto = true;  //Reasignando la variable
+
+disponible = true; //Le damos el valor a la variable
+
+var producto1 = 'Monitor',
+    disponible1 = true,
+    categoria = 'Monitores';
+
+var disponible_;
+
+//Estilos para las variables
+var nombre_producto = 'Monitor 4K'; // underscore
+var nombreProducto = 'Monitor 4K' // Camelcase
+var NombreProducto = 'Monitor 4k' // Pascal Case
+var nombreproducto = 'Monitor 4k'; // lowercase
+
+console.log(producto);
+
