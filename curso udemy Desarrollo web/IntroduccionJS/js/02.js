ac7a93843a3f21ad4fd56819a8b59aa025a509b7
William Emmanuel Mazariego Orellana <83888568+Will-Trucker@users.noreply.github.com>
2023-05-18 15:46:10 -0600

    Variables con Let y Const

--- curso udemy Desarrollo web/IntroduccionJS/js/02.js
+++ curso udemy Desarrollo web/IntroduccionJS/js/02.js
@@ -1,22 +1,22 @@
 // Variables con Let
-var producto = 'Audifonos Gamer'; //Iniciar la Variable y asignarle valor
+let producto = 'Audifonos Gamer'; //Iniciar la Variable y asignarle valor
 
-var disponible; //Iniciar variable pero sin ningun valor
+let disponible; //Iniciar variable pero sin ningun valor
 
 //producto = true;  //Reasignando la variable
 
 disponible = true; //Le damos el valor a la variable
 
-var producto1 = 'Monitor',
+let producto1 = 'Monitor',
     disponible1 = true,
     categoria = 'Monitores';
 
-var disponible_;
+let disponible_;
 
 //Estilos para las variables
-var nombre_producto = 'Monitor 4K'; // underscore
-var nombreProducto = 'Monitor 4K' // Camelcase
-var NombreProducto = 'Monitor 4k' // Pascal Case
-var nombreproducto = 'Monitor 4k'; // lowercase
+let nombre_producto = 'Monitor 4K'; // underscore
+let nombreProducto = 'Monitor 4K' // Camelcase
+let NombreProducto = 'Monitor 4k' // Pascal Case
+let nombreproducto = 'Monitor 4k'; // lowercase
 
 console.log(producto);
---
+++ curso udemy Desarrollo web/IntroduccionJS/js/03.js
@@ -0,0 +1,27 @@
+// Variables con Let
+
+const producto = 'Audifonos Gamer'; //Iniciar la Variable y asignarle valor
+
+let disponible = true; //Iniciar variable pero sin ningun valor
+
+//producto = true;  //Reasignando la variable
+
+disponible = true; //Le damos el valor a la variable
+
+
+
+const producto1 = 'Monitor',
+    disponible1 = true,
+    categoria = 'Monitores';
+
+
+
+const disponible_ = true;
+
+//Estilos para las variables
+const nombre_producto = 'Monitor 4K'; // underscore
+const nombreProducto = 'Monitor 4K' // Camelcase
+const NombreProducto = 'Monitor 4k' // Pascal Case
+const nombreproducto = 'Monitor 4k'; // lowercase
+
+console.log(producto);
