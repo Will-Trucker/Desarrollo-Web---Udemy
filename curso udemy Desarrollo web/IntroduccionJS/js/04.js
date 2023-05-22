e1adbadb71b588ac0e87695dc24f0bcc5b6e5db5
William Emmanuel Mazariego Orellana <83888568+Will-Trucker@users.noreply.github.com>
2023-05-22 10:43:14 -0600

    Metodos para los strings

--- curso udemy Desarrollo web/IntroduccionJS/js/04.js
+++ curso udemy Desarrollo web/IntroduccionJS/js/04.js
@@ -1,5 +1,17 @@
 // String o cadena de texto
-const producto = 'Monitor de 20 pulgadas"';
+const tweet = 'Aprendiendo Javascript con el curso de Desarrollo Web Completo';
+const producto2 = 'Monitor HD';
+const email = 'correo@mail.com'
 
+//Length es para extension
+console.log(tweet.length);
+console.log(producto2);
 
-console.log(producto);
+//IndexOf (retorna posicion)
+console.log(tweet.indexOf('Javascript'));
+console.log(producto2.indexOf('Tablet'));
+console.log(email.indexOf('@'));
+
+//Include (retorna true o false)
+console.log(tweet.includes('Javascript'));
+console.log(producto2.includes('Tablet'));
