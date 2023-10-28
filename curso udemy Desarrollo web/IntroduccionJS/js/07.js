17bdceee74b979b43b00a9b5af740a925a9913f5
Will-Trucker <willitomarley@outlook.es>
2023-10-28 17:00:50 -0600

    Controlar el orden de las operaciones Concatenación y Template Strings

--- curso udemy Desarrollo web/IntroduccionJS/js/07.js
+++ curso udemy Desarrollo web/IntroduccionJS/js/07.js
@@ -1,1 +1,19 @@
 //Orden de las operaciones
+
+let resultado;
+
+resultado = (20 + 30) * 2; //En este caso se aplica que las operaciones del parentesis se resuelvan primero
+
+resultado = (600 + 600) * 1.05;
+
+//console.log(resultado);
+
+
+//Incrementos
+
+let puntaje = 10;
+
+//Incremento en 1 ++
+puntaje += 10;
+
+console.log(puntaje);
---
+++ curso udemy Desarrollo web/IntroduccionJS/js/08.js
@@ -0,0 +1,10 @@
+const nombre = 'Will';
+const email = 'correo@mail.com';
+
+
+//Concatenacion
+console.log("Nombre Cliente: " + nombre + " Email: " + email);
+
+
+//Template Strings - String Literals 
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
